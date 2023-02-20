258f5f1506406b5fc7464ee44dca17680c02c379
Vishal Manjhi <vmanjhi98@gmail.com>
2023-02-20 05:54:47 +0530

    gjg

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import { CounterStateContext } from '../counterContext';
+import React, { useReducer } from 'react'
 import '../styles/App.css';
-import { CounterList } from './CounterList';
 const App = () => {
-
+  
+  const intialTaskState = {count: 0};
+  const [taskState, dispatch] = useReducer(reducer, intialTaskState);
+  
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'add':
+           return {count: state.count +1};
+      case 'delete':
+           return {count: state.count > 0 ? state.count - 1 : 0};
+      case 'deleteAll':
+           return {count: 0};
+        default:
+           throw new Error();
+    }
+}
+    
   return (
-    <CounterStateContext>
-      <div id="main">
-        <CounterList />
-      </div>
-    </CounterStateContext>
-
+    <div id="main">
+      <h2>Task Counter</h2>
+      <button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
+      <button onClick={() => dispatch({type: 'delete'})}  id="delTaskBtn">Delete a task</button>
+      <button onClick={() => dispatch({type: 'deleteAll'})}  id="delAllTaskBtn">Delete all tasks</button>
+      <h3 className="counter">Number of Tasks : {taskState.count}</h3>
+    </div>
   )
 }
 
